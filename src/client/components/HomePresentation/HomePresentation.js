27a00a57d74ef0fbd5fe76b3ece5162f851b79ff
Valentina Méndez <valentina.mdz@gmail.com>
2021-06-14 12:19:48 +0200

    Set correct favicon and add image to home

--- src/client/components/HomePresentation/HomePresentation.js
+++ src/client/components/HomePresentation/HomePresentation.js
@@ -6,14 +6,19 @@ export const HomePresentation = () => {
   return (
     <>
       <div className="home-presentation">
-        Hola! I am <span className="hover-text">Valentina,</span>{" "}
+        I am <span className="hover-text">Valentina,</span>{" "}
         <div className="hover-image">
-          <img src={Retrato} alt="portrait" loading="lazy"/>
+          <img src={Retrato} alt="portrait" loading="lazy" />
         </div>
         <span className="wavy">a creative web developer and designer</span>. I
-        am interested in building meaningful and{" "}
-        <span className="font-change">well crafted</span> projects with
+        am interested in building <span className="font-change">beautiful</span>
+        , meaningful and <span className="font-change">well crafted</span>{" "}
+        projects with
         <span className="color-change"> inspiring people.</span>
+        <br />
+        <a href="/projects" className="projects-link">
+          See projects
+        </a>
       </div>
     </>
   );
--- src/client/containers/Home/Home.js
+++ src/client/containers/Home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { HomePresentation } from "../../components/HomePresentation/HomePresentation";
 import "./Home.css";
+import Hola from "../../assets/images/hola.svg";
 
 export const Home = () => {
   return (
     <div className="home">
       <HomePresentation></HomePresentation>
-      <></>
+      <img src={Hola} alt="hola" className="hola"></img>
     </div>
   );
 };
