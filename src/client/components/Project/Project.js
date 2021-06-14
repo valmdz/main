7fc61d2aeb93f68145071b7ee8617edda1c6fc0c
Valentina Méndez <valentina.mdz@gmail.com>
2021-06-14 09:49:40 +0200

    Update Projects.js structure

--- src/client/components/Project/Project.js
+++ src/client/components/Project/Project.js
@@ -10,39 +10,42 @@ export const Project = () => {
       <div className="projects-group">
         {Projects.projects.projects.map((p) => (
           <div className="project-element" key={p.id}>
-            <h1>{p.title}</h1>
-            <img src={p.image} alt="project preview" />
-            <h2>{p.about}</h2>
-            <h2>
-              <span className="highlight">Year: </span> {p.year}
-            </h2>
-            <h2>
-              <span className="highlight">Setting:</span> {p.setting}
-            </h2>
-            <h2 className="keywords">
-              <span className="highlight">Stack:</span>{" "}
-              {p.keywords.map((keyword) => " " + keyword + " / ")}
-            </h2>
-            <div className="icons-link">
-              {p.links.website && (
-                <a
-                  href={p.links.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={Website} alt="icon" className="icon" />
-                </a>
-              )}
+            <img src={p.image} alt="project preview" loading="lazy" />
 
-              {p.links.github && (
-                <a
-                  href={p.links.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={Github} alt="icon" className="icon" />
-                </a>
-              )}
+            <div>
+              <h1>{p.title}</h1>
+              <h2>{p.about}</h2>
+              <h2>
+                <span className="highlight">Year: </span> {p.year}
+              </h2>
+              <h2>
+                <span className="highlight">Setting:</span> {p.setting}
+              </h2>
+              <h2 className="keywords">
+                <span className="highlight">Stack:</span>{" "}
+                {p.keywords.map((keyword) => " " + keyword + " / ")}
+              </h2>
+              <div className="icons-link">
+                {p.links.website && (
+                  <a
+                    href={p.links.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={Website} alt="icon" className="icon" />
+                  </a>
+                )}
+
+                {p.links.github && (
+                  <a
+                    href={p.links.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={Github} alt="icon" className="icon" />
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         ))}
